4c47877d5b83582543a20611c7ee87ec27a5ec0d
DrBretto <59483898+DrBretto@users.noreply.github.com>
2022-03-11 18:18:02 -0500

    cleaned up some code

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ApiProvider } from './contexts/ApiContext';
-import { ReverbContextProvider } from './contexts/ReverbContext';
 import App from './routes/App';
 
 ReactDOM.render(
   <BrowserRouter>
     <ApiProvider>
-      {/* <ReverbContextProvider> */}
-        <App />
-      {/* </ReverbContextProvider> */}
+      <App />
     </ApiProvider>
   </BrowserRouter>,
   document.getElementById('root')
